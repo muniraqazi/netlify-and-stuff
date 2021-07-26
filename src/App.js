e01f3a71dd530c0fb11174ae80c8f5d0a98ed0d1
Munira <muniraqazi98@gmail.com>
2021-07-26 12:11:40 -0400

    testing netlify deploy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          minimal viable mondays 
+          minimal viable mondays !!! ¯\_(ツ)_/¯ 
         </p>
       </header>
     </div>
